7a4e8c56ab00898e4ab76acb134b900cfad4a6a9
Felipe <felipe@MacBook-Pro-de-Felipe-2.local>
2020-05-07 09:51:15 -0400

    Actividad clase

---
+++ ActividadClase/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+//using System.xml.Serialatation;
+
+namespace ActividadClase
+{
+    [Serializable()]
+    public class Person:ISerializable
+    {
+        //List<Person> persons = new List<Person>();
+        private Dictionary<int, List<string>> persons;
+        private string name;
+        private string lastName;
+        private string age;
+
+        public Person()
+        {
+        }
+        public void AddPerson()
+        {
+            Console.Write("Nombre:");
+            name = Console.ReadLine();
+            Console.Write("Apellido:");
+            lastName = Console.ReadLine();
+            Console.Write("Edad:");
+            age = Console.ReadLine();
+            AddPerson(new List<string>(){ name, lastName, age});
+        }
+
+        public void AddPerson(List<string> data)
+        {
+                this.persons.Add(persons.Count + 1, data);
+        }
+
+        public void SeePersons()
+        {
+            foreach (List<string> user in this.persons.Values)
+            {
+                Console.WriteLine(user);
+            }
+
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", name);
+            info.AddValue("LastName", lastName);
+            info.AddValue("Age", age);
+
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            name = (string)info.GetValue("Name", typeof(string));
+            name = (string)info.GetValue("LastName", typeof(string));
+            name = (string)info.GetValue("Age", typeof(string));
+
+        }
+
+    }
+}
---
+++ ActividadClase/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+//using System.Xml.Serialization;
+
+namespace ActividadClase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            Stream stream = File.Open("PersonData.dat",FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, person);
+            stream.Close();
+            stream = File.Open("PersonData.dat", FileMode.Open);
+            binaryFormatter = new BinaryFormatter();
+            person = (Person)binaryFormatter.Deserialize(stream);
+            stream.Close();
+
+            string[] options = { "Agregar Persona", "Guardar y Cargar", "Ver Personas", "Salir" };
+            bool selectingMenu = true;
+            ConsoleKeyInfo selectedOption;
+            while (selectingMenu)
+            {
+                Console.Clear();
+                int optionIndex = 1;
+                foreach (string option in options)
+                {
+                    Console.WriteLine($"{optionIndex} - {option}");
+
+                    optionIndex += 1;
+                }
+                selectedOption = Console.ReadKey();
+                System.Threading.Thread.Sleep(300);
+                Console.Clear();
+                switch (selectedOption.Key.ToString())
+                {
+                    case "D1":
+                        Console.WriteLine("Agregar Persona");
+                        person.AddPerson();
+                        break;
+                    case "D2":
+                        Console.WriteLine("Almacenar y Cargar");
+                        
+                        break;
+                    case "D3":
+                        Console.WriteLine("Lista de personas");
+                        person.SeePersons();
+                        Console.ReadKey();
+                        break;
+                    case "D4":
+                        selectingMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ingrese option valida...");
+                        System.Threading.Thread.Sleep(900);
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
